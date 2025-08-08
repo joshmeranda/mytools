9c6ae961f90548fce8d50a097ad796d558ee699d
joshmeranda <joshua.meranda@gmail.com>
2025-08-08 10:31:40 -0400

    fix file bak name construction

--- common/bak.sh
+++ common/bak.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
-prefix="~"
+if [ $# -eq 0 ]; then
+    echo "expected files but found none"
+    exit 1
+fi
 
 while :; do
     case "$1" in
         '-p' | '--prefix' )
+            if [ $# -lt 2 ]; then
+                echo "expected prefix but found none"
+                exit 1
+            fi
+
             prefix="$2";
             shift
             ;;
 
         '-s' | '--suffix' )
+            if [ $# -lt 2 ]; then
+                echo "expected suffix but found none"
+                exit 1
+            fi
+
             suffix="$2"
             shift
             ;;
@@ -20,6 +33,10 @@ while :; do
     shift
 done
 
+if [ -z "$prefix" ] && [ -z "$suffix" ]; then
+    prefix="~"
+fi
+
 for path in "$@"; do
-    cp --recursive "$path" "${prefix}${path}${suffix}"
+    cp --recursive "$path" "$(dirname $path)/$prefix$(basename $path)$suffix"
 done
