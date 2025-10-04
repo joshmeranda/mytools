8f3d8e705eeb062080adac78591ed14df6280b8f
joshmeranda <joshua.meranda@suse.com>
2025-10-04 11:16:36 -0400

    dynamic boxes for renovate

--- rancher/renovate.sh
+++ rancher/renovate.sh
@@ -9,7 +9,7 @@ check_pr() {
 	approved=$(grep APPROVED <<< "$reviews" | wc --lines)
 	changes_requested=$(grep CHANGES_REQUESTED <<< "$reviews" | wc --lines)
 
-	printf '│ %+50s │ %2d✔ %2d✖ │\n' $(jq --raw-output .html_url <<< $pr) ${approved:-0} ${changes_requested:-0}
+	printf "│ %+${max_html_url_length}s │ %2d✔ %2d✖ │\n" $(jq --raw-output .html_url <<< $pr) ${approved:-0} ${changes_requested:-0}
 }
 
 DAY_RANGE=${DAY_RANGE:-7}
@@ -25,22 +25,35 @@ if [ -n "$DEBUG" ]; then
 	echo "DAY_RANGE: $DAY_RANGE"
 	echo "    REPOS: $REPOS"
 	echo "    query: $query"
+
+	# Need to unset this to avoid unwanted output from gh
 	DEBUG=''
 fi
 
 pr_urls=$(gh api --method GET search/issues --paginate --slurp \
 	--raw-field q="$query" \
-	| jq -c -M -r '.[].items[].pull_request.url')
+	| jq -c -M -r '.[].items[] | .pull_request.url + "\t" + .pull_request.html_url')
 
-printf '┌%s┐\n' $(printf '─%.0s' {0..61})
-printf '│ %+50s │ %+7s │\n' 'pull request' reviews
-printf '├%s┼%s┤\n' $(printf '─%.0s' {0..51}) $(printf '─%.0s' {0..8})
+pr_api_urls=$(cut -f 1 <<< "$pr_urls")
+pr_html_urls=$(cut -f 2 <<< "$pr_urls")
 
-for pr_url in $pr_urls; do
-	check_pr $pr_url &
+max_html_url_length=11
+for html_url in $pr_html_urls; do
+	l=${#html_url}
+	if [ $l -gt $max_html_url_length ]; then
+		max_html_url_length=$l
+	fi
+done
+
+printf '┌%s┐\n' $(printf '─%.0s' $(seq 0 $(( $max_html_url_length + 2 + 7 + 2 )) ))
+printf "│ %+${max_html_url_length}s │ %+7s │\n" 'pull request' reviews
+printf '├%s┼%s┤\n' $(printf '─%.0s' $(seq 0 $(( $max_html_url_length + 1 )) )) $(printf '─%.0s' {0..8})
+
+for api_url in $pr_api_urls; do
+	check_pr $api_url &
 done
 wait
 
-printf '└%s┘\n' $(printf '─%.0s' {0..61})
+printf '└%s┘\n' $(printf '─%.0s' $(seq 0 $(( $max_html_url_length + 2 + 7 + 2 )) ))
 
 # │ ├ ─ ┼ ┤ ┌ ┐ └ ┘
