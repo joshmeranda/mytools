186fc993342e2253dbfc4cf29b1d8d2d76c5dcaf
joshmeranda <joshua.meranda@gmail.com>
2025-10-07 18:22:14 -0400

    add exceptions for test requirements

--- common/test/test_common.py
+++ common/test/test_common.py
@@ -1,10 +1,17 @@
 import pathlib
 
 
+EXCLUDED_SCRIPTS=[
+	"wf.sh",
+	"foff.sh",
+	"ticker.sh",
+]
+
+
 def test_each_common_script_has_test():
 	common = pathlib.Path("common")
 	common_test = common / "test"
 
 	for child in pathlib.Path("common").iterdir():
-		if child.is_file():
+		if child.is_file() and not child.name in EXCLUDED_SCRIPTS:
 			assert (common_test / f"test_{child.stem.replace("-", "_")}.py").exists()
