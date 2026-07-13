0268c7298e243b7469034faef8843dc681689f6b
joshmeranda <joshua.meranda@gmail.com>
2026-07-13 14:45:53 -0400

    add  --help with usage information

--- kubernetes/controller.bash
+++ kubernetes/controller.bash
@@ -60,7 +60,30 @@ while [ "$#" -gt 0 ]; do
 			flags+=" --selector $2"
 			shift
 			;;
-		
+
+		-h | --help)
+			echo "$(basename $0): [-n <namespace>] [-A] [--field-selector <field selector>]
+                 [-f <path>] [-R <true|false>] [--raw <uri>] [-l <selector>]
+                 <SCRIPT> <KIND> [NAME]
+			
+$(basename $0) allows you to run k8s controllers straight from the terminal or
+in your scripts. For example, to run the SCRIPT at './update-tracker.bash' each
+time a pod is updated run:
+	$(basename $0) ./update-tracker.bash pod
+
+For each update a new instance of SCRIPT is started, but will wait until the
+previous instance is complete. Be warned that this may eat up computing
+resources if there are many triggering events. To help prevent this run your
+own checks and validations early.
+
+For more information on the flags specifiec above see 'kubectl get --help'.
+
+Note: No attempt is made to limit what events will be received by the callback
+      script. If you receive an event for a resource and send an update
+      to that same resource, you will receive another event for that update."
+			exit
+			;;
+
 		* )
 			if [ -z "$callback" ]; then
 				callback="$1"
